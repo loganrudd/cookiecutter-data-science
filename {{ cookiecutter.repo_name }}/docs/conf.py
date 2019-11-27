df56225994103092e7bb683f6d4c20c758831f5c
Logan Rudd <loganrudd@gmail.com>
2019-11-26 20:01:14 -0600

    Remove calls to project_name

--- {{ cookiecutter.repo_name }}/docs/conf.py
+++ {{ cookiecutter.repo_name }}/docs/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# {{ cookiecutter.project_name }} documentation build configuration file, created by
+# {{ cookiecutter.repo_name }} documentation build configuration file, created by
 # sphinx-quickstart.
 #
 # This file is execfile()d with the current directory set to its containing dir.
@@ -41,7 +41,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'{{ cookiecutter.project_name }}'
+project = u'{{ cookiecutter.repo_name }}'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -185,8 +185,6 @@ latex_elements = {
 latex_documents = [
     ('index',
      '{{ cookiecutter.repo_name }}.tex',
-     u'{{ cookiecutter.project_name }} Documentation',
-     u"{{ cookiecutter.author_name }}", 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -215,8 +213,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', '{{ cookiecutter.repo_name }}', u'{{ cookiecutter.project_name }} Documentation',
-     [u"{{ cookiecutter.author_name }}"], 1)
+    ('index', '{{ cookiecutter.repo_name }}', 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -229,8 +226,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    ('index', '{{ cookiecutter.repo_name }}', u'{{ cookiecutter.project_name }} Documentation',
-     u"{{ cookiecutter.author_name }}", '{{ cookiecutter.project_name }}',
+    ('index', '{{ cookiecutter.repo_name }}',
      '{{ cookiecutter.description }}', 'Miscellaneous'),
 ]
 
